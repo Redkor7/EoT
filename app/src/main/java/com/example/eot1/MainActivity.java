e78e5d4cbf665a692c00fb12f8012f2b19b22ec7
Redkor7 <stv.first.one@gmail.com>
2023-04-25 18:04:08 +0300

    v0.1.0

--- app/src/main/java/com/example/eot1/MainActivity.java
+++ app/src/main/java/com/example/eot1/MainActivity.java
@@ -6,31 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnStr;
+    Button start, exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnStr = findViewById(R.id.button5);
+        start = findViewById(R.id.b_start);
+        exit = findViewById(R.id.b_exit);
         Intent intent = new Intent(this, MainGameActivity.class);
 
-        btnStr.setOnClickListener(new View.OnClickListener() {
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showText();
                 startActivity(intent);
             }
         });
-    }
 
-    private  void showText(){
-        Toast.makeText(this, "Good bro", Toast.LENGTH_LONG).show();
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
